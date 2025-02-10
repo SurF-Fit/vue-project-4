8ea899dbf4a787d1a1fe763896c57e681b49bf00
SurF-Fit <kkulmenev@gmail.com>
2025-02-10 10:36:12 +0700

    Самое начало...

---
+++ my-app/src/components/header.vue
@@ -0,0 +1,32 @@
+<template>
+  <header>
+    <div class="wrapper">
+      <a href="#">Каталог товаров</a>
+      <a href="#">Регистрация</a>
+      <a href="#">Авторизация</a>
+      <a href="#">Корзина</a>
+      <a href="#">Оформленные заказы</a>
+    </div>
+  </header>
+</template>
+
+<style>
+.wrapper {
+  margin: 0 auto;
+  gap: 20px;
+  display: flex;
+}
+
+a{
+  color: black;
+  text-decoration: none;
+}
+
+a:hover {
+  text-decoration: underline;
+}
+</style>
+
+<script>
+
+</script>
--- my-app/src/main.js
+++ my-app/src/main.js
@@ -1,6 +1,8 @@
 import { createApp } from 'vue'
-import App from './App.vue'
 import router from './router'
 import store from './store'
+import App from './App.vue'
+import Header from './components/header.vue'
 
 createApp(App).use(store).use(router).mount('#app')
+createApp(Header).mount('#header')
