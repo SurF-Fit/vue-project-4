3d29ad70ff7c238c5ebf989183017262d439e8bb
SurF-Fit <kkulmenev@gmail.com>
2025-02-11 16:27:55 +0700

    Начата работа над стилем корзины

--- my-app/src/views/basket.vue
+++ my-app/src/views/basket.vue
@@ -2,7 +2,8 @@
   <div>
     <h2>Корзина</h2>
     <ul>
-      <li v-for="item in cartItems" :key="item.id">
+      <li class="item" v-for="item in cartItems" :key="item.id">
+        <img :src="getImageUrl(item.image)" alt="Изображение товара" class="item-image" />
         {{ item.name }} - {{ item.price }} руб. (Количество: {{ item.length }})
         <button @click="minusItem(item.id)">-</button>
         <button @click="plassItem(item.id)">+</button>
@@ -14,7 +15,27 @@
 </template>
 
 <style scoped>
+  .item {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 20px;
+  }
 
+  img{
+    width: 50px;
+    height: 50px;
+  }
+
+  button{
+    background-color: white;
+    width: max-content;
+  }
+
+  button:hover{
+    background-color: #d3d3d3;
+    cursor: pointer;
+  }
 </style>
 
 <script>
@@ -27,6 +48,10 @@ export default {
   },
   methods: {
     ...mapActions(['removeFromCart', 'clearCart', 'plassItem', 'minusItem']),
+    getImageUrl(imagePath) {
+      const baseUrl = 'http://lifestealer86.ru/';
+      return `${baseUrl}${imagePath}`;
+    }
   },
 };
 </script>
